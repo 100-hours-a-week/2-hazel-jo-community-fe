ad45daf0942c35dce850e931b2b10eadda892182
groom <hayaon0603@naver.com>
2024-11-07 11:52:58 +0900

    [feat]: 드롭다운 메뉴 선택 시 페이지 이동

---
+++ public/js/common.js
@@ -0,0 +1,19 @@
+const optionEditProfile = document.querySelector('#editProfile');
+const optionEditPassword = document.querySelector('#editPassword');
+const optionLogout = document.querySelector('#logout');
+
+function dropdownMenu() {
+    optionEditProfile.addEventListener('click', () => {
+        location.href = '/page/edit profile.html';
+    });
+    optionEditPassword.addEventListener('click', () => {
+        location.href = '/page/edit password.html';
+    });
+    optionLogout.addEventListener('click', () => {
+        location.href = '/logout';
+    });
+}
+
+dropdownMenu();
+
+
