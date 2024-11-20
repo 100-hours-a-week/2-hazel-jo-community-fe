567100bac2120a4c5003d2bdb90c473783565fbd
groom <hayaon0603@naver.com>
2024-11-20 10:14:50 +0900

    [feat]: 로그아웃 기능 구현 및 로컬 스토리지 초기화

--- public/js/common.js
+++ public/js/common.js
@@ -1,20 +1,27 @@
+import { logoutUser } from '../api/user-api.js';
+
 const optionEditProfile = document.querySelector('#editProfile');
 const optionEditPassword = document.querySelector('#editPassword');
 const optionLogout = document.querySelector('#logout');
 
+// 드롭다운 메뉴 선택 시 이동
 function dropdownMenu() {
     optionEditProfile.addEventListener('click', () => {
         location.href = '/page/edit profile.html';
     });
     optionEditPassword.addEventListener('click', () => {
+        
         location.href = '/page/edit password.html';
     });
     optionLogout.addEventListener('click', () => {
+        logoutUser();
+        clearLocalStorage();
         location.href = '/page/Log in.html';
     });
 }
 
-function setProfileImage() {
+// 프로필 이미지 설정
+export function setProfileImage() {
     const profileImage = document.querySelector('.login-icon img');
     let userProfileImage = localStorage.getItem('profileImage');
 
@@ -39,7 +46,14 @@ function setProfileImage() {
     }
 }
 
+// 로그아웃 시 로컬 스토리지 초기화
+export function clearLocalStorage() {
+    localStorage.clear();
+}
+
+
 dropdownMenu();
 setProfileImage();
+
 
 
