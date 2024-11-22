0adb90e952e2d5880bab8be18ab33c7718ed29c0
groom <hayaon0603@naver.com>
2024-11-23 05:09:32 +0900

    [feat] 댓글 API 기능 추가 : 댓글 작성, 수정, 삭제

---
+++ public/api/comments-api.js
@@ -0,0 +1,111 @@
+const baseUrl = 'http://localhost:5000/comments';
+
+// 댓글 불러오기 
+export async function loadComments(postId) {
+  try {
+        const response = await fetch(`${baseUrl}/${postId}`, {
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        });
+        
+        
+        if(!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.message || '댓글 불러오기 실패');
+        }
+        
+        const data = await response.json();
+        return data.comments;
+   
+    } catch (error) {
+        console.error('Fetch 오류:', error);
+        return [];
+    }
+  }
+
+// 댓글 생성 
+export async function createComment(postId, commentData) {
+    try {
+        const response = await fetch(`${baseUrl}/${postId}`, {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(commentData)
+        });
+
+        if(!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.message || '댓글 생성 실패');
+        }
+
+        const newComment = await response.json();
+        return newComment;
+    } catch (error) {
+        console.error('댓글 생성 오류: ', error);
+        throw error;
+    }
+}
+
+// 댓글 삭제 
+export async function deleteComment(commentId) {
+    try {
+        const response = await fetch(`${baseUrl}/${commentId}`, {
+            method: 'DELETE',
+            credentials: 'include',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if(!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.message || '댓글 삭제 실패');
+        }
+
+        return true;
+    } catch (error) {
+        console.error('댓글 삭제 오류: ', error);
+        throw error;
+    }
+}
+
+// 댓글 수정 
+export async function updateComment(commentId, commentData) {
+    try {
+        const response = await fetch(`${baseUrl}/${commentId}`, {
+            method: 'PATCH',
+            credentials: 'include',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(commentData)
+        });
+
+        if(!response.ok) {
+            const errorData = await response.json();
+    
+            // 세션 만료 시 로그인 페이지로 리다이렉트
+            if(response.status === 401) {
+                alert('로그인이 필요합니다. 로그인 페이지로 이동합니다.');
+                window.location.href = '/page/login.html';
+                return;
+            }
+            throw new Error(errorData.message || '댓글 수정 실패');
+        }
+    
+        const updatedComment = await response.json();
+        return updatedComment;
+    } catch (error) {
+        console.error('댓글 수정 오류: ', error);
+        throw error;
+    }
+}
