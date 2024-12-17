764b82c8289f4273d0e105a641287d841c2d9304
groom <hayaon0603@naver.com>
2024-12-17 23:27:55 +0900

    [feat] num이 undefined 또는 null일 경우를 처리하는 조건문 추가

--- public/utils/convertUtils.js
+++ public/utils/convertUtils.js
@@ -1,6 +1,7 @@
 // 숫자를 K 단위로 변환
 export const convertK = (num) => {
-    if(num >= 1000) {
+    if (num === undefined || num === null) return '0';
+    if (num >= 1000) {
         return (num / 1000).toFixed(0) + 'k';
     }
     return num.toString();
