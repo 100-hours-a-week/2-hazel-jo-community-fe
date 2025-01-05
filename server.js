21b3da708463cc2905425d86715dc13dbe1167f0
groom <hayaon0603@naver.com>
2025-01-06 00:48:23 +0900

    [fix] 서버 외부 네트워크 접근 가능하도록 수정

--- server.js
+++ server.js
@@ -54,6 +54,9 @@ app.get('/public/page/Log in.html', (req, res) => {
 });
 
 // 서버 시작
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
