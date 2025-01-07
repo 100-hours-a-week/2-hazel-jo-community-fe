c4081859fbca95ff98fdc1a26e4db3bdf9a9955e
groom <hayaon0603@naver.com>
2025-01-07 17:15:57 +0900

    [fix] API URL을 새로운 IP로 변경

--- public/utils/config.js
+++ public/utils/config.js
@@ -4,7 +4,7 @@ function getApiUrl() {
         return 'http://localhost:5001';
     } else {
         console.log('Using deployed API URL');
-        return 'http://54.180.86.12:5001';
+        return 'http://43.203.172.138:5001';
     }
 }
 
